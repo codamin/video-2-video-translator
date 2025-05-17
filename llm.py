79469ba673d7062e45ee239251e88a824bf8fe2a
Amin Asadi Sarijalou <43009143+codamin@users.noreply.github.com>
2025-05-17 10:48:00 +0200

    Update llm.py

--- llm.py
+++ llm.py
@@ -4,19 +4,22 @@ os.environ["TOGETHER_API_KEY"] = "tgp_v1_-WVlTgjJk3ZFeT8mG-DbvkIXwZ4SL4SJxBsfTZo
 from together import Together
 client = Together()
 model_name = "google/gemma-2b-it"
-response = client.chat.completions.create(
-    model=model_name,
-    messages=[
-        {
-        "role": "system",
-        "content": "You are an expert in translation from English to French. You get a paragraph in English as input and translate it to French. Only output the translation, and say nothing more."
-        },
-        {
-        "role": "user",
-        "content": english_text
-        }
-    ],
-    stream=False,
-    )
 
-return response.choices[0].message.content
+def translate_text_to_text(source_text, source_language, target_language):
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {
+            "role": "system",
+            "content": f"You are an expert in translation from {source_language} to {target_language}. You are very precise and accurate. \
+                Only output the translation, and say nothing more."
+            },
+            {
+            "role": "user",
+            "content": source_text
+            }
+        ],
+        stream=False,
+        )
+
+    return response.choices[0].message.content
